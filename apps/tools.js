cef5ceffa7e4b74998ce206d269d00328aa487eb
Joseph Hentges <hentgesj@augsburg.edu>
2019-09-22 23:36:33 -0500

    Added the tools file, bringing in helper functions from main

---
+++ apps/tools.js
@@ -0,0 +1,24 @@
+// database
+const { dbs } = require('../main/configs/db/db_connection');
+// tools and helpers
+const { checkKey } = require('../main/controllers/api-tools/check-key');
+const { checkValues } = require('../main/controllers/api-tools/check-values');
+const { checkQuery } = require('../main/controllers/api-tools/check-query');
+const { updateLogs } = require('../main/controllers/tools/update.logs');
+const { updateLastLogged } = require('../main/controllers/tools/update.LastLoggedIn');
+const nodemailer = require('../main/controllers/tools/nodemailer');
+// Email Templates
+const { verifyEmail } = require('../main/views/email.templates/verify.email');
+const { passwordResetEmail } = require('../main/views/email.templates/passwordReset.email');
+
+module.exports = {
+  dbs,
+  checkKey,
+  checkValues,
+  checkQuery,
+  updateLogs,
+  updateLastLogged,
+  nodemailer,
+  verifyEmail,
+  passwordResetEmail
+};
