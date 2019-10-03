270adaa628586ce588c39fe30bf8a0e387ef6c77
Joseph Hentges <josephhentges@Josephs-MacBook-Pro.local>
2019-10-03 11:26:44 -0500

    Added comments to the password reset and account verification typedefs files

--- apps/Account-Helpers/controllers/typeDefs/AccountVerification.typedefs.js
+++ apps/Account-Helpers/controllers/typeDefs/AccountVerification.typedefs.js
@@ -1,3 +1,13 @@
+/**
+ * Author
+ * Joseph Hentges
+ * October 2019
+ * https://joeyhentges.com
+ * 
+ * The typedefs file for Account Verification graphQL queries and mutations.
+ * This is a schema for what data can be returned from the data, and what functions are 'callable'
+ */
+
 const { buildSchema } = require('graphql');
 
 const defs = `
@@ -23,4 +33,5 @@ const defs = `
     }
 `;
 
+// build the schema from the defs string and export it
 module.exports.AccountVerificationTypedefs = buildSchema(`${defs}`);
